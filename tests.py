b1a4723c5a2756c09eea41149c2784652e6b4b4a
CWorld <cworld0@qq.com>
2024-05-19 18:57:29 +0800

    perf: :zap: complete the code

--- tests.py
+++ tests.py
@@ -210,9 +210,9 @@ def test_all():
 
 def get_all_prefixed_errors():
     return {
-        # **prefix_errors(test_action_and_result)(),
-        # **prefix_errors(test_hill_climbing_instrumented)(),
-        # **prefix_errors(test_hill_climbing_sideways)(),
+        **prefix_errors(test_action_and_result)(),
+        **prefix_errors(test_hill_climbing_instrumented)(),
+        **prefix_errors(test_hill_climbing_sideways)(),
         **prefix_errors(test_hill_climbing_random_restart)(),
     }
 
